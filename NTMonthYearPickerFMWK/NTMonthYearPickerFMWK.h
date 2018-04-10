6d9f6a9760b2159ee8270768f874cdf05215376b
Antonino Urbano <ant@urbano.me>
2018-04-10 11:16:55 -0400

    Fixed an error to make the framework importable as-is.

---
+++ NTMonthYearPickerFMWK/NTMonthYearPickerFMWK.h
@@ -0,0 +1,19 @@
+//
+//  NTMonthYearPickerFMWK.h
+//  NTMonthYearPickerFMWK
+//
+//  Created by Antonino Urbano on 2018-04-10.
+//  Copyright © 2018 Arun Nair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for garbagio.
+FOUNDATION_EXPORT double NTMonthYearPickerFMWKVersionNumber;
+
+//! Project version string for garbagio.
+FOUNDATION_EXPORT const unsigned char NTMonthYearPickerFMWKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NTMonthYearPickerFMWK/PublicHeader.h>
+#import <NTMonthYearPickerFMWK/NTMonthYearPicker.h>
+
